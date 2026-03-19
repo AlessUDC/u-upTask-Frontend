a8c4276567a35a7d198d60ce94e013f3ac201193
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-19 18:29:28 -0500

    Implementando funcion getProjects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,4 +1,4 @@
-import type { ProjectFormData } from "@/types/index"
+import { dashboardProjectSchema, type ProjectFormData } from "@/types/index"
 import api from "@/lib/axios"
 import { isAxiosError } from "axios"
 
@@ -6,6 +6,22 @@ export async function createProject(formData: ProjectFormData) {
     try {
         const { data } = await api.post("/projects", formData)
         return data
+    } catch (error) {
+        // Si el error de este tipo exacto existe, retorna el error
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects')
+        const response = dashboardProjectSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
+        
     } catch (error) {
         // Si el error de este tipo exacto existe, retorna el error
         if (isAxiosError(error) && error.response) {
