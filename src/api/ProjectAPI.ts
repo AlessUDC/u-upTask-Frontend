4da72094ad3166532778962e8fbe191795427ff8
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-19 19:43:22 -0500

    Implementar funciones de obtención y actualización de proyectos en la API

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,4 +1,4 @@
-import { dashboardProjectSchema, type ProjectFormData } from "@/types/index"
+import { dashboardProjectSchema, type Project, type ProjectFormData } from "@/types/index"
 import api from "@/lib/axios"
 import { isAxiosError } from "axios"
 
@@ -22,6 +22,35 @@ export async function getProjects() {
             return response.data
         }
         
+    } catch (error) {
+        // Si el error de este tipo exacto existe, retorna el error
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        return data
+    } catch (error) {
+        // Si el error de este tipo exacto existe, retorna el error
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+type ProjectAPIType = {
+    formData: ProjectFormData
+    projectId: Project['_id']
+}
+
+export async function updateProject({ formData, projectId }: ProjectAPIType) {
+    try {
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
     } catch (error) {
         // Si el error de este tipo exacto existe, retorna el error
         if (isAxiosError(error) && error.response) {
