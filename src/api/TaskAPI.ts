a38c29c6a96ec2d18e97ffc74edbf9ff585f391a
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-22 22:56:00 -0500

    Implementar validación de datos y función para actualizar el estado de las tareas

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from "axios"
 import api from "@/lib/axios"
-import type { Project, Task, TaskFormData } from "../types"
+import { TaskSchema, type Project, type Task, type TaskFormData } from "../types"
 
 type TaskAPI = {
     formData : TaskFormData
     projectId : Project['_id']
     taskId : Task['_id']
+    status : Task['status']
 }
 
 export async function createTask({formData, projectId}: Pick<TaskAPI, 'formData' | 'projectId'>) {
@@ -24,7 +25,11 @@ export async function getTaskById({projectId, taskId}: Pick<TaskAPI, 'projectId'
     try {
         const url = `/projects/${projectId}/tasks/${taskId}`
         const { data } = await api(url)
-        return data
+        
+        const response = TaskSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
     } catch (error) {
         if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
@@ -48,6 +53,18 @@ export async function deleteTask({ projectId, taskId }: Pick<TaskAPI, 'projectId
     try {
         const url = `/projects/${projectId}/tasks/${taskId}`
         const { data } = await api.delete<string>(url)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function updateStatus({ projectId, taskId, status }: Pick<TaskAPI, 'projectId' | 'taskId' | 'status'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}/status`
+        const { data } = await api.post<string>(url, { status })
         return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
