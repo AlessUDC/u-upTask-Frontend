03f8c736fdd819f0fab924d8f96fd65f02161d14
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-29 13:57:49 -0500

    Incorporar interceptor de axios para manejar automáticamente el token de autenticación

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,16 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
 
+// Antes del request se ejecuta esta función para que se incluya el token en cada request
+// Esto es útil para que no tengamos que incluir el token en cada request que hagamos
+// Por ejemplo en ProjectAPI.ts en cada método api.get, api.post, etc.
+api.interceptors.request.use( config => {
+  // Obtenemos el token del localStorage
+  const token = localStorage.getItem('AUTH_TOKEN')
+  // Si el token existe, lo incluimos en el header de la request
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 export default api
