7d08b6dfe4288ecd3a3242becaa90aadc97b4767
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-30 21:44:17 -0500

    Definir rutas para la administración del equipo

--- src/router.tsx
+++ src/router.tsx
@@ -11,6 +11,7 @@ import ConfirmAccountView from "./views/auth/ConfirmAccountView"
 import RequestCodeView from "./views/auth/RequestCodeView"
 import ForgotPasswordView from "./views/auth/ForgotPasswordView"
 import NewPasswordView from "./views/auth/NewPasswordView"
+import ProjectTeamView from "./views/projects/ProjectTeamView"
 
 export function Router() {
   return (
@@ -21,6 +22,7 @@ export function Router() {
           <Route path="/projects/create" element={<CreateProjectView />} />
           <Route path="/projects/:projectId" element={<ProjectDetailView />} />
           <Route path="/projects/:projectId/edit" element={<EditProjectView />} />
+          <Route path="/projects/:projectId/team" element={<ProjectTeamView />} />
         </Route>
 
         <Route element={<AuthLayout />}>
