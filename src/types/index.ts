349111e2521d1f237bab360d185968e50b95a8c2
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-30 21:40:37 -0500

    Definir esquemas y tipos para la gestión de colaboradores

--- src/types/index.ts
+++ src/types/index.ts
@@ -63,3 +63,14 @@ export const dashboardProjectSchema = z.array(
 
 export type Project = z.infer<typeof ProjectSchema>
 export type ProjectFormData = Pick<Project, "projectName" | "clientName" | "description">
+
+// ---- Team ----
+const teamMemberSchema = userSchema.pick({
+    name: true,
+    email: true,
+    _id: true
+})
+
+export const teamMembersSchema = z.array(teamMemberSchema)
+export type TeamMember = z.infer<typeof teamMemberSchema>
+export type TeamMemberForm = Pick<TeamMember, 'email'>
